68dfa8776a39e4b68f7d6b934596a8dac39061f6
pratik <pratikgupte16@gmail.com>
2020-02-07 11:39:21 +0100

    mod indivs for ann

--- ann_tipping_points/individual.h
+++ ann_tipping_points/individual.h
@@ -3,83 +3,41 @@
 
 #include <random>
 #include "rnd.hpp"
+#include "ann.h"
+
+using namespace ann;
+
+// specify ann structure
+using Ann = Network<float,
+	Layer< Neuron<1, activation::rtlu>, 3>, // for now, 1 input for env cues
+	Layer< Neuron<3, activation::rtlu>, 3>,
+	Layer< Neuron<3, activation::rtlu>, 1>  // one output phenotype value
+>;
 
 struct Individual {
 
-  Individual() : h(1.f), mismatch(0.f) {
-  
-    const auto& dist1 = std::uniform_real_distribution<float>(0.f, 1.f);
-    const auto& dist2 = std::uniform_real_distribution<float>(-1.f, 1.f);
-    const auto& dist3 = std::uniform_real_distribution<float>(-2.f, 2.f);
+	Individual() : mismatch(0.f), ann_dev(0.f), ann_life(0.f) {};
 
-    s = dist1(rnd::reng);
-    a = dist1(rnd::reng);
+	Ann ann_dev, ann_life;
 
-    I01 = dist2(rnd::reng);
-    I02 = dist2(rnd::reng);
+	float I_baseline, I_realized;
+	float mismatch;
 
-    b1 = dist3(rnd::reng);
-    b2 = dist3(rnd::reng);
-    
+	// response to development cue
+	void update_I_g(const float C) {
+		Ann::input_t inputs_g;
+		inputs_g[0] = C;
+		auto output_g = ann_dev(inputs_g);
+		I_baseline = output_g[0];
+	}
 
-  }
-
-
-  void update_I_t(const float C) {
-    if (s > 0.5) {
-      if (a > 0) {
-        if (h_coin) {
-          I_realized = b1 * C + I01;
-        }
-        else {
-          I_realized = b2 * C + I02;
-        }
-      }
-
-    }
-  
-  }
-  void update_I_g(float C) {
-  
-    h_coin = std::bernoulli_distribution(h)(rnd::reng);
-
-    if (s > 0.5) {
-      if (h_coin) {
-        I_realized = b1 * C + I01;
-      }
-      else {
-        I_realized = b2 * C + I02;
-      }
-
-    }
-    else {
-      if (h_coin) {
-        I_realized = I01;
-      }
-      else {
-        I_realized = I02;
-      }
-
-    }
-  
-  }
-
-
-
-  //genes
-  float h;
-  float I01;
-  float I02;
-  float b1;
-  float b2;
-  float s;
-  float a;
-
-  //lifetime params
-  bool h_coin;
-  float I_realized;
-  float mismatch;
-
+	// lifetime modification of baseline value
+	void update_I_t(const float C) {
+		Ann::input_t inputs_t;
+		inputs_t[0] = C;
+		auto output_t = ann_dev(inputs_t);
+		I_realized = I_baseline + output_t[0];
+	}
 };
 
 
