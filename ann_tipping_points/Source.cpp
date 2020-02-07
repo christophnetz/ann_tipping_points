ef9bdbf2a8f5f015738f8dd701343d5449dd09e8
pratik <pratikgupte16@gmail.com>
2020-02-07 12:03:35 +0100

    add reprod and dev cue response

--- ann_tipping_points/Source.cpp
+++ ann_tipping_points/Source.cpp
@@ -7,64 +7,117 @@
 #include "individual.h"
 
 
-
-
-
-
 int main() {
 
-  std::vector<Individual> pop(5000); // population size: 5000
-  //Varied Parameters
-  // A and B were varied between 0 and 1 (A+B = 1) to investigate deterministic vs stochastic env., not found to have a great effect 
-  
-  float A = 1.f;   //Deterministic scaling constant, default value 
-  float B = 0.f;   //stochastic scaling constant, default value
+	const int popsize = 5000;
+	std::vector<Individual> pop(5000); // population size: 5000
+	//Varied Parameters
+	// A and B were varied between 0 and 1 (A+B = 1) to investigate deterministic vs stochastic env., not found to have a great effect 
 
-  //R between 1 and 100000, P between 0 and 1
-  float R = 10.f;  //Environmental variation
-  float P = 1.f;   //predictability
-  ///
-  const auto& env_dist = std::uniform_real_distribution<float>(-1.f, 1.f); // not explicitly stated in botero 2015
-  float E;
-  float Cue;
+	float A = 1.f;   //Deterministic scaling constant, default value 
+	float B = 0.f;   //stochastic scaling constant, default value
 
-  int gmax = 50000;
-  int tmax = 5;
+	//R between 1 and 100000, P between 0 and 1
+	float R = 10.f;  //Environmental variation
+	float P = 1.f;   //predictability
+	///
+	const auto& env_dist = std::uniform_real_distribution<float>(-1.f, 1.f); // not explicitly stated in botero 2015
+	float E;
+	float Cue;
 
-  std::cout << "Hello world" << std::endl;
+	int gmax = 50000;
+	int tmax = 5;
 
-  for (int g = 0; g < gmax; g++) {
-
-    for (int t = 0; t < tmax; t++) {
-      //update environment
-      E = A * std::sin( (2 * M_PI * (g * tmax + t))/(tmax * R)) + B * env_dist(rnd::reng);
-      //calculate cue
-      Cue = std::normal_distribution<float>(P * E, (1 - P) / 3)(rnd::reng);
-      /// Is Cue calculated once for the whole population, or per individual?
-
-      //individual update
-      for (int i = 0; i < pop.size(); ++i) {
-
-        pop[i].update_I_t(Cue);
-
-      }
+	// reproduction function
 
 
-      //phenotypic mismatch
+	std::cout << "Hello world" << std::endl;
+
+	for (int g = 0; g < gmax; g++) {
+
+		for (int t = 0; t < tmax; t++) {
+			//update environment
+			E = A * std::sin((2 * M_PI * (g * tmax + t)) / (tmax * R)) + B * env_dist(rnd::reng);
+			//calculate cue
+			Cue = std::normal_distribution<float>(P * E, (1 - P) / 3)(rnd::reng);
+			/// Is Cue calculated once for the whole population, or per individual?
+
+			//individual update during lifetime
+			for (int i = 0; i < pop.size(); ++i) {
+
+				pop[i].update_I_t(Cue);
+
+			}
 
 
-    }
-
-    //Reproduction
+			//phenotypic mismatch
 
 
+		}
 
-  }
+		//Reproduction -- could be a function but oh well
+		{
+			// make fitness vec
+			std::vector<double> fitness_vec;
+			float max = 0.f; float min = 0.f;
+			for (int a = 0; a < popsize; a++)
+			{
+				fitness_vec.push_back(static_cast<double> (pop[a].mismatch));
+			}
+
+			// make temp pop vector, position and energy vectors
+			std::vector<Individual> pop2(popsize);
+
+			// assign parents
+			for (int a = 0; a < popsize; a++) {
+
+				std::discrete_distribution<> weighted_lottery(fitness_vec.begin(), fitness_vec.end());
+				int parent_id = weighted_lottery(rnd::reng);
+				
+				// replicate ann_dev
+				pop2[a].ann_dev = pop[parent_id].ann_dev;
+				pop2[a].ann_life = pop[parent_id].ann_life;
+				
+				// reset mismatch
+				pop2[a].mismatch = 0.f;
+				// process a baseline -- this is development
+				pop2[a].update_I_g(Cue);
+
+				// mutate ann_dev
+				for (auto& w : pop2[a].ann_dev) {
+					std::bernoulli_distribution mut_event(0.001); // mutation probability
+					// probabilistic mutation of ANN
+					if (mut_event(rnd::reng)) {
+						std::cauchy_distribution<double> m_shift(0.0, 0.01); // how much of mutation
+						w += static_cast<float> (m_shift(rnd::reng));
+					}
+				}
+
+				// mutate ann_life
+				for (auto& w : pop2[a].ann_life) {
+					std::bernoulli_distribution mut_event(0.001); // mutation probability
+					// probabilistic mutation of ANN
+					if (mut_event(rnd::reng)) {
+						std::cauchy_distribution<double> m_shift(0.0, 0.01); // how much of mutation
+						w += static_cast<float> (m_shift(rnd::reng));
+					}
+				}
+
+			}
+
+			//overwrite old pop
+			pop = pop2;
+
+		}
+
+
+
+	}
 
 
 
 
-  return 0;
+	return 0;
 }
 
 
