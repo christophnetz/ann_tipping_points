83d5524fa8b122a45932ddfdad97eff87b9d732b
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-08 07:40:03 +0100

    move funcs to ind cpp

--- ann_tipping_points/individual.h
+++ ann_tipping_points/individual.h
@@ -24,44 +24,11 @@ struct Individual {
 	float I_baseline, I_realized;
 	float mismatch;
 
-	// response to development cue
-	void update_I_g(const float C) {
-		Ann::input_t inputs_g;
-		inputs_g[0] = C;
-		auto output_g = ann_dev(inputs_g);
-		I_baseline = output_g[0];
-	}
-
-	// lifetime modification of baseline value
-	void update_I_t(const float C) {
-		Ann::input_t inputs_t;
-		inputs_t[0] = C;
-		auto output_t = ann_dev(inputs_t);
-		I_realized = I_baseline + output_t[0];
-	}
-
-	// get mismatch
+	void update_I_g(const float C);
+	void update_I_t(const float C);
 	inline void update_mismatch(const float E) { mismatch += abs(E - I_realized); }
-
-	// get fitness
 	float calculate_fitness(float kd, float ka, float tau);
-
-	// get reaction norm
-	std::vector<float> get_reaction(const std::vector<float> vec_cues){
-		std::vector<float> vec_reaction;
-
-		for(size_t i_cue = 0; i_cue < static_cast<size_t>(vec_cues.size()); i_cue++){
-			// get ann output
-			Ann::input_t inputs_t;
-			inputs_t[0] = vec_cues[i_cue];
-			auto output_t = ann_dev(inputs_t);
-			I_realized = I_baseline + output_t[0];
-
-			vec_reaction.push_back(I_realized);
-		}
-		return vec_reaction;
-	}
-
+	std::vector<float> get_reaction(const std::vector<float> vec_cues);
 };
 
 
