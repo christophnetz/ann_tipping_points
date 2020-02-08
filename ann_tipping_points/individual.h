9ab0e8afb1a7f2d61e2b24fa98b7e4f508770262
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-08 07:34:24 +0100

    get ann reaction norms

--- ann_tipping_points/individual.h
+++ ann_tipping_points/individual.h
@@ -43,6 +43,25 @@ struct Individual {
 	// get mismatch
 	inline void update_mismatch(const float E) { mismatch += abs(E - I_realized); }
 
+	// get fitness
+	float calculate_fitness(float kd, float ka, float tau);
+
+	// get reaction norm
+	std::vector<float> get_reaction(const std::vector<float> vec_cues){
+		std::vector<float> vec_reaction;
+
+		for(size_t i_cue = 0; i_cue < static_cast<size_t>(vec_cues.size()); i_cue++){
+			// get ann output
+			Ann::input_t inputs_t;
+			inputs_t[0] = vec_cues[i_cue];
+			auto output_t = ann_dev(inputs_t);
+			I_realized = I_baseline + output_t[0];
+
+			vec_reaction.push_back(I_realized);
+		}
+		return vec_reaction;
+	}
+
 };
 
 
