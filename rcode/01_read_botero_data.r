ffefd99ec16617ab41564cf7171c2053cc0668b6
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-13 11:54:05 +0100

    changes to r code

--- rcode/01_read_botero_data.r
+++ rcode/01_read_botero_data.r
@@ -34,23 +34,30 @@ process_cue <- function(h, I01, I02, b1, b2, s, a){
   
   # work through parameters getting slope and intercept
   if(s <= 0.5){
-    if(h < 1){
+    if(h < 1 && h > 0){
       # name strategy
       data_rnorm <- data.table(intercept = c(I01, I02),
                                slope = 0, strategy = c("dbh1", "dbh2"))
       
-    }else{
+    }else if (h >= 1 ){
       data_rnorm <- data.table(intercept = I01, slope = 0, strategy = "cbh/adt")
     }
+    else if (h  <= 0){
+      data_rnorm <- data.table(intercept = I02, slope = 0, strategy = "cbh/adt")
+    }
   }else{
-    if(h < 1){
+    if(h < 1 && h > 0){
       # name strategy
       data_rnorm <- data.table(intercept = c(I01, I02), slope = c(b1, b2),
                                strategy = c("pbh02", "pbh02"))
       
-    }else{
+    }else if (h >= 1 ){
       data_rnorm <- data.table(intercept = I01, slope = b1,
-                               strategy = ifelse(a < 0, "dev_plast", "fen_plast"))
+                               strategy = ifelse(a <= 0, "dev_plast", "fen_plast"))
+    }
+    else if (h <= 0 ){
+      data_rnorm <- data.table(intercept = I02, slope = b2,
+                               strategy = ifelse(a <= 0, "dev_plast", "fen_plast"))
     }
   }
   
@@ -69,12 +76,12 @@ data <- select(data, -agents) %>%
 
 # plot figure
 ggplot(data)+
-  geom_abline(aes(intercept = intercept, slope = slope), 
+  geom_abline(aes(intercept = intercept, slope = slope, col = strategy), 
               alpha = 0.1)+
   scale_x_continuous(limits = c(0,1))+
   scale_y_continuous(limits = c(0,1))+
   coord_cartesian(xlim=c(-1,1), ylim=c(-1, 1))+
-  # scale_colour_manual(values = c("black", "red", "green", "green", "yellow", "blue"))+
+  scale_colour_manual(values = c("black", "green", "green", "blue", "red", "yellow"))+
   facet_grid(P~R, labeller = label_both, as.table = FALSE)+
   theme(strip.placement = "inside")+
   theme_bw()
--- rcode/02_read_ann_data.r
+++ rcode/02_read_ann_data.r
@@ -14,27 +14,30 @@ library(viridis)
 
 #### load data ####
 # list data files
-data_files <- list.files("data/", full.names = TRUE, pattern = "ann")
+{
+  data_files <- list.files("data/", full.names = TRUE, pattern = "ann")
+  
+  # get params from filename
+  parameters <- str_extract_all(data_files, regex("[0-9].[0-9]")) %>% 
+    map(function(vals){
+      tibble(R = vals[1], P = vals[2])
+    }) %>% 
+    bind_rows()
+  
+  # read data
+  data <- lapply(data_files, fread)
+  
+  # attach parameters to data
+  data <- mutate(parameters, agents = data)
+  
+  # unnest data
+  data <- unnest(data, cols = agents)}
 
-# get params from filename
-parameters <- str_extract_all(data_files, regex("[0-9].[0-9]")) %>% 
-  map(function(vals){
-    tibble(R = vals[1], P = vals[2])
-  }) %>% 
-  bind_rows()
-
-# read data
-data <- lapply(data_files, fread)
-
-# attach parameters to data
-data <- mutate(parameters, agents = data)
-
-# unnest data
-data <- unnest(data, cols = agents)
+#df <- count(data, R, P, cue, resp)
 
 # plot by combo
 ggplot(data)+
-  geom_path(aes(x = cue, y = resp, group = ind), 
-            col = "red", alpha = 0.01)+
+  # geom_line(aes(x = cue, y = resp - baseline, group = ind), col = "red", alpha=0.01)+
+  #geom_hline(aes(yintercept = baseline), col = "blue", alpha=0.01)+
+  geom_line(aes(x = cue, y = resp, group = ind), alpha=0.01)+
   facet_grid(R~P, labeller = label_both)
- 
