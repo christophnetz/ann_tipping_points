b72b0965abb0018d3456d9f089ddf1f991a590f2
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-13 15:48:46 +0100

    conv process_cue to matrix

--- rcode/01_read_botero_data.r
+++ rcode/01_read_botero_data.r
@@ -36,52 +36,53 @@ process_cue <- function(h, I01, I02, b1, b2, s, a){
   if(s <= 0.5){
     if(h < 1 && h > 0){
       # name strategy
-      data_rnorm <- data.table(intercept = c(I01, I02),
-                               slope = 0, strategy = c("dbh1", "dbh2"))
+      data_rnorm <- rbind(c(I01, 0, "dbh1"), 
+                          c(I02, 0, "dbh2"))
       
     }else if (h >= 1 ){
-      data_rnorm <- data.table(intercept = I01, slope = 0, strategy = "cbh/adt")
+      data_rnorm <- rbind(c(I01, 0, "cbh/adt"))
     }
     else if (h  <= 0){
-      data_rnorm <- data.table(intercept = I02, slope = 0, strategy = "cbh/adt")
+      data_rnorm <- rbind(c(I02,0,"cbh/adt"))
     }
   }else{
     if(h < 1 && h > 0){
       # name strategy
-      data_rnorm <- data.table(intercept = c(I01, I02), slope = c(b1, b2),
-                               strategy = c("pbh02", "pbh02"))
+      data_rnorm <- rbind(c(I01, b1, "pbh01"), c(I02, b2, "pbh02"))
       
     }else if (h >= 1 ){
-      data_rnorm <- data.table(intercept = I01, slope = b1,
-                               strategy = ifelse(a <= 0, "dev_plast", "fen_plast"))
+      data_rnorm <- rbind(c(I01, b1, ifelse(a <= 0, "dev_plast", "fen_plast")))
     }
     else if (h <= 0 ){
-      data_rnorm <- data.table(intercept = I02, slope = b2,
-                               strategy = ifelse(a <= 0, "dev_plast", "fen_plast"))
+      data_rnorm <- rbind(c(I02, b2, ifelse(a <= 0, "dev_plast", "fen_plast")))
     }
   }
   
+  colnames(data_rnorm) <- c("intercept", "slope","strat")
   return(data_rnorm)
   
 }
 
 #### get slope and intercept of evolved agents ####
 data <- mutate(data, r_norms = map(agents, function(df){
-  pmap_df(df[,2:ncol(df)], process_cue)
+  temp_mat <- pmap(df[,2:ncol(df)], process_cue)
+  temp_mat <- reduce(temp_mat, rbind)
+  temp_mat <- as.data.table(temp_mat)
+  temp_mat[,`:=`(slope = as.numeric(slope), intercept = as.numeric(intercept))]
 }))
 
 # remove agents
-data <- select(data, -agents) %>% 
+data2 <- select(data, -agents) %>% 
   unnest(cols = "r_norms")
 
 # plot figure
-ggplot(data)+
-  geom_abline(aes(intercept = intercept, slope = slope, col = strategy), 
+ggplot(data2)+
+  geom_abline(aes(intercept = intercept, slope = slope, col = strat), 
               alpha = 0.1)+
   scale_x_continuous(limits = c(0,1))+
   scale_y_continuous(limits = c(0,1))+
   coord_cartesian(xlim=c(-1,1), ylim=c(-1, 1))+
-  scale_colour_manual(values = c("black", "green", "green", "blue", "red", "yellow"))+
+  scale_colour_manual(values = c("black", "green", "green", "blue", "red", "yellow", "yellow"))+
   facet_grid(P~R, labeller = label_both, as.table = FALSE)+
   theme(strip.placement = "inside")+
   theme_bw()
