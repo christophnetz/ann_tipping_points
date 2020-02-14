c89240e381c02d8014b9b4e0dfc98642be6a5454
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-14 11:36:49 +0100

    add red box for native regime

--- rcode/03_extinction_ann.r
+++ rcode/03_extinction_ann.r
@@ -11,16 +11,31 @@ library(tidyr)
 # plotting libs
 library(ggplot2)
 library(viridis)
+library(ggthemes)
 
 #### load data ####
 data <- fread("data/extinction_data.csv")
 
+# potential changes
+tot_data <- expand(data, R, P, R_new, P_new) %>% 
+  left_join(data)
+
 # plot tiled facets
-ggplot(data)+
-  geom_tile(aes(x = R_new, y = P_new, fill = gen_extinct))+
+ggplot(tot_data)+
+  geom_tile(aes(x = R_new, y = P_new, fill = gen_extinct),
+            size = 0.1)+
+  geom_tile(aes(x = R, y = P), fill = NA, col = "red", lwd = 1)+
   facet_grid(P~R, labeller = label_both, 
              switch = "both", as.table = FALSE)+
-  scale_fill_viridis_c()+
-  coord_cartesian(expand = FALSE)+
-  theme_bw()+
-  theme(strip.placement = "outside")
+  scale_fill_viridis_c(option = "E",
+                       na.value = viridis(1, begin = 1))+
+  coord_cartesian(expand = F)+
+  scale_y_continuous(breaks = c(0,0.5,1))+
+  scale_x_continuous(breaks = c(0:5))+
+  theme_few()+
+  labs(x = NULL, y = NULL)+
+  theme(strip.placement = "outside",
+        legend.position = "none", 
+        strip.background = element_rect(fill = "grey90", 
+                                        colour = "black"))
+  
