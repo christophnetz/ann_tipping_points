ff3db913f4f20f38fa2a0f279105467d2d1e2ca7
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-17 10:16:30 +0100

    add prelim pop trend code

--- rcode/03_extinction_ann.r
+++ rcode/03_extinction_ann.r
@@ -10,15 +10,11 @@ library(tidyr)
 
 # plotting libs
 library(ggplot2)
-library(viridis)
+library(scico)
 library(ggthemes)
 
-#### load data ####
+#### extinction landscape ####
 data <- fread("data/extinction_data.csv")
-
-# potential changes
-tot_data <- expand(data, R, P, R_new, P_new) %>% 
-  left_join(data)
 
 # plot tiled facets
 ggplot(tot_data)+
@@ -39,3 +35,32 @@ ggplot(tot_data)+
         strip.background = element_rect(fill = "grey90", 
                                         colour = "black"))
   
+
+#### population trends ####
+pop_data <- fread("data/pop_trend_data.csv")
+
+# calculate if R and P are increased or not
+pop_data[,`:=`(deltaP = P_new - P, deltaR = R_new - R)]
+pop_data[,shape:=ifelse(deltaP > 0, 24, 25)]
+
+# convert pop to proportion and add const val
+pop_data[,prop:=(popsize/5e3)+(20*deltaP)]
+
+ggplot(pop_data[deltaP %% 0.2 == 0,])+
+  geom_line(aes(x = gen, y = popsize, col = deltaR,
+                group = interaction(deltaR, deltaP)),
+            size = 0.2, alpha = 0.9)+
+  
+  # geom_point(aes(x = gen, y = prop,
+  #                fill = deltaR), alpha = 0.5, size = 0.1)+
+  scale_size(range = c(0.1, 0.5))+
+  scale_fill_scico(palette = "berlin")+
+  scale_colour_scico(palette = "berlin")+
+  facet_grid(R~P, labeller = label_both,
+             switch = "both", as.table = FALSE)+
+  theme_few()+
+  coord_equal(ratio = 0.2)+
+  theme(strip.placement = "outside",
+        legend.position = "none", 
+        strip.background = element_rect(fill = "grey90", 
+                                        colour = "black"))
