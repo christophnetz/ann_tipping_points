1e23ff78f0179eca010fa9750ef78c3afc3601f1
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-17 11:09:21 +0100

    minor changes

--- rcode/03_extinction_ann.r
+++ rcode/03_extinction_ann.r
@@ -16,26 +16,21 @@ library(ggthemes)
 #### load data ####
 data <- fread("data/extinction_data.csv")
 
-# potential changes
-tot_data <- expand(data, R, P, R_new, P_new) %>% 
-  left_join(data)
-
 # plot tiled facets
-ggplot(tot_data)+
+ggplot(data)+
   geom_tile(aes(x = R_new, y = P_new, fill = gen_extinct),
             size = 0.1)+
   geom_tile(aes(x = R, y = P), fill = NA, col = "red", lwd = 1)+
   facet_grid(P~R, labeller = label_both, 
              switch = "both", as.table = FALSE)+
-  scale_fill_viridis_c(option = "E",
-                       na.value = viridis(1, begin = 1))+
-  coord_cartesian(expand = F)+
+  scale_fill_viridis_c(option = "C")+
+  coord_fixed(ratio = 5, expand = F)+
   scale_y_continuous(breaks = c(0,0.5,1))+
   scale_x_continuous(breaks = c(0:5))+
   theme_few()+
   labs(x = NULL, y = NULL)+
   theme(strip.placement = "outside",
-        legend.position = "none", 
+        legend.position = "none",
         strip.background = element_rect(fill = "grey90", 
                                         colour = "black"))
   
